1c975e4f4df9f79ddc0d332dd5922bf98818fa84
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-08-06 20:00:48 -0300

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reactJava',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
